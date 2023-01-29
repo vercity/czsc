a5034eb8982fc2916e2cf8ca40b12b577afdad87
guyeqi <vercity.gu@ximalaya.com>
2023-01-29 08:59:52 +0800

    补完回测结果

--- examples/ts_stocks_trader_backtest.py
+++ examples/ts_stocks_trader_backtest.py
@@ -62,8 +62,6 @@ n3bResult = []
 n5bResult = []
 n21bResult = []
 
-
-
 def dingmessage(dingMessage):
     return
     # 请求的URL，WebHook地址
@@ -98,6 +96,10 @@ strategyFolderPath = os.path.join(data_path, hashlib.md5(strategyName.encode(enc
 if not os.path.exists(strategyFolderPath):
     os.mkdir(strategyFolderPath)
 
+isBackTestResult = True
+# ssss = np.load(os.path.join(strategyFolderPath, "btStock") + '.npy', allow_pickle=True).item()
+btStock = {}
+
 def backtest(stocks):
     global dikaiNextDay, gaokaiNextDay
     global maxZF
@@ -116,8 +118,10 @@ def backtest(stocks):
         if os.path.exists(stockPath):
             buyPointsDt = read_pkl(stockPath)
             hasCache = True
-        else:
-            continue
+
+        if isBackTestResult:
+            hasCache = True
+
 
         if not hasCache:
             # 回测暂时只和fromDate有关，别的都是读缓存的记录
@@ -159,65 +163,81 @@ def backtest(stocks):
                 continue
 
         print("所有开多日期是: " + str(buyPointsDt))
-        kaicangPrice = 0
-        for oneDay in buyPointsDt:
-            # 前后看100天
-            probar = dc.pro_bar(ts_code=oneStock, start_date=trade_dates[trade_dates.index(oneDay) - 100],
-                                       end_date=trade_dates[trade_dates.index(oneDay) + 100], raw_bar=False)
-            ma5 = probar["close"].rolling(5).mean()
-            ma10 = probar["close"].rolling(10).mean()
-            currentDay = probar[probar["trade_date"] == oneDay]
-            if currentDay.empty:
-                print("empty dataframe")
-                continue
-            nextDay = probar.iloc[currentDay.index+1]
-            preclose = nextDay["pre_close"].iloc[0]
-            open = nextDay["open"].iloc[0]
-            if kaicangPrice == 0:
-                kaicangPrice = open
-                print("{}, {} 开盘价开仓: {}".format(nextDay["ts_code"].iloc[0], trade_dates[trade_dates.index(oneDay) + 1], str(open)))
-            # 计算第二天是高开还是低开
-            if open >= preclose:
-                gaokaiNextDay += 1
-            else:
-                dikaiNextDay += 1
-            # 买点出现后60天的数据
-            next60DatasDF = probar.iloc[currentDay.index.values[0]+1:currentDay.index.values[0]+61]
-            # 买点出现后最高价出现的index
-            ind = next60DatasDF["close"].idxmax()
-            # 最高价当天的具体数据
-            maxDayData = next60DatasDF.loc[ind]
-            maxZhangfu = (maxDayData["close"] - preclose) / preclose
-            if maxZhangfu > maxZF:
-                maxZF = maxZhangfu
-                print("最大涨幅股票:" + oneStock)
-                print("最大涨幅是: " + str(maxZhangfu))
-                print("买点出现时间: " + oneDay + " 第: " + str(ind-100) + " 天, 收盘价: " + str(maxDayData["close"]))
-            # 计算在出信号后第二天开盘价买入，持有一天后的开盘价收益(%)
-            hold1Day = currentDay["n1b"].iloc[0] / 100
-            n1bResult.append(hold1Day)
-            # 计算在出信号后第二天开盘价买入，持有3天后的开盘价收益(%)
-            hold3Day = currentDay["n3b"].iloc[0] / 100
-            n3bResult.append(hold3Day)
-            # 计算在出信号后第二天开盘价买入，5天后的开盘价收益(%)
-            hold5Day = currentDay["n5b"].iloc[0] / 100
-            n5bResult.append(hold5Day)
-            # 计算在出信号后第二天开盘价买入，21天后的开盘价收益(%)
-            hold21Day = currentDay["n21b"].iloc[0] / 100
-            n21bResult.append(hold21Day)
-    # 统计汇总
-    print("总共交易 {} 次".format(gaokaiNextDay+dikaiNextDay))
-    print("高开占比 {}".format(gaokaiNextDay / (gaokaiNextDay + dikaiNextDay)))
-    print("1天就卖，平均赚 {}".format(str(np.nanmean(n1bResult))))
-    print("3天就卖，平均赚 {}".format(str(np.nanmean(n3bResult))))
-    print("5天就卖，平均赚 {}".format(str(np.nanmean(n5bResult))))
-    print("21天就卖，平均赚 {}".format(str(np.nanmean(n21bResult))))
+        if isBackTestResult:
+            kaicangPrice = 0
+            for oneDay in buyPointsDt:
+                if oneDay in btStock.keys():
+                    stockList = btStock[oneDay]
+                    stockList.append(oneStock)
+                else:
+                    btStock[oneDay] = [oneStock]
+                # 前后看100天
+                probar = dc.pro_bar(ts_code=oneStock, start_date=trade_dates[trade_dates.index(oneDay) - 100],
+                                           end_date=trade_dates[trade_dates.index(oneDay) + 100], raw_bar=False)
+                ma5 = probar["close"].rolling(5).mean()
+                ma10 = probar["close"].rolling(10).mean()
+                currentDay = probar[probar["trade_date"] == oneDay]
+                if currentDay.empty:
+                    print("empty dataframe")
+                    continue
+                nextDay = probar.iloc[currentDay.index+1]
+                preclose = nextDay["pre_close"].iloc[0]
+                openPrice = nextDay["open"].iloc[0]
+                if kaicangPrice == 0:
+                    kaicangPrice = openPrice
+                    print("{}, {} 开盘价开仓: {}".format(nextDay["ts_code"].iloc[0], trade_dates[trade_dates.index(oneDay) + 1], str(open)))
+                # 计算第二天是高开还是低开
+                if openPrice >= preclose:
+                    gaokaiNextDay += 1
+                else:
+                    dikaiNextDay += 1
+                # 买点出现后60天的数据
+                next60DatasDF = probar.iloc[currentDay.index.values[0]+1:currentDay.index.values[0]+61]
+                # 买点出现后最高价出现的index
+                ind = next60DatasDF["close"].idxmax()
+                # 最高价当天的具体数据
+                maxDayData = next60DatasDF.loc[ind]
+                maxZhangfu = (maxDayData["close"] - preclose) / preclose
+                if maxZhangfu > maxZF:
+                    maxZF = maxZhangfu
+                    print("最大涨幅股票:" + oneStock)
+                    print("最大涨幅是: " + str(maxZhangfu))
+                    print("买点出现时间: " + oneDay + " 第: " + str(ind-100) + " 天, 收盘价: " + str(maxDayData["close"]))
+                # 计算在出信号后第二天开盘价买入，持有一天后的开盘价收益(%)
+                hold1Day = currentDay["n1b"].iloc[0] / 100
+                n1bResult.append(hold1Day)
+                # 计算在出信号后第二天开盘价买入，持有3天后的开盘价收益(%)
+                hold3Day = currentDay["n3b"].iloc[0] / 100
+                n3bResult.append(hold3Day)
+                # 计算在出信号后第二天开盘价买入，5天后的开盘价收益(%)
+                hold5Day = currentDay["n5b"].iloc[0] / 100
+                n5bResult.append(hold5Day)
+                # 计算在出信号后第二天开盘价买入，21天后的开盘价收益(%)
+                hold21Day = currentDay["n21b"].iloc[0] / 100
+                n21bResult.append(hold21Day)
+    if isBackTestResult:
+        # 统计汇总
+        print("总共交易 {} 次".format(gaokaiNextDay + dikaiNextDay))
+        print("高开占比 {}".format(gaokaiNextDay / (gaokaiNextDay + dikaiNextDay)))
+        print("1天就卖，平均赚 {}".format(str(np.nanmean(n1bResult))))
+        print("3天就卖，平均赚 {}".format(str(np.nanmean(n3bResult))))
+        print("5天就卖，平均赚 {}".format(str(np.nanmean(n5bResult))))
+        print("21天就卖，平均赚 {}".format(str(np.nanmean(n21bResult))))
+
+        tf = open(os.path.join(strategyFolderPath, "btStock") + '.json', "w")
+        json.dump(btStock, tf)
+        tf.close()
+        # save_pkl(os.path.join(strategyFolderPath, "btStock") + '.npy', stockPath)
+        # np.save(os.path.join(strategyFolderPath, "btStock") + '.npy', btStock)
 
 if __name__ == '__main__':
-    # process = [Process(target=backtest, args=(allStockCodes[0:1000],)),
-    #            Process(target=backtest, args=(allStockCodes[1000:2000],)),
-    #            Process(target=backtest, args=(allStockCodes[2000:3000],)),
-    #            Process(target=backtest, args=(allStockCodes[3000:4000],)), ]
-    # [p.start() for p in process]
-    # [p.join() for p in process]
-    backtest(allStockCodes)
+    if isBackTestResult:
+        backtest(allStockCodes)
+    else:
+        process = [Process(target=backtest, args=(allStockCodes[0:1000],)),
+                   Process(target=backtest, args=(allStockCodes[1000:2000],)),
+                   Process(target=backtest, args=(allStockCodes[2000:3000],)),
+                   Process(target=backtest, args=(allStockCodes[3000:4000],)), ]
+        [p.start() for p in process]
+        [p.join() for p in process]
+
